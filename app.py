9fe7136c5da0e33a7155b7e55591637122d0f7ee
jensenspecht <specht11@terpmail.umd.edu>
2026-03-10 20:41:56 -0400

    Completed first half of flask fundamentals project.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,5 +19,22 @@ def about():  # put application's code here
 def about_css():  # put application's code here
     return render_template('about-css.html')
 
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    print('Subject entered: ' + request.args.get('subject'))
+    print('Course number entered: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return render_template('contact.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
